9bd5398610108d3b30b95a49e65b9a11c041b883
krishpranav <krisna.pranav@gmail.com>
2021-05-20 18:13:31 +0530

    [+] added LyricsViewController class [+]

--- lyricsify/LyricsViewController.swift
+++ lyricsify/LyricsViewController.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+class LyricsViewController: NSViewController {
+    
+    @IBOutlet weak var trackTitleView: NSTextField!
+    @IBOutlet weak var trackLyricsView: NSTextField!
+    @IBOutlet weak var trackProgressView: NSProgressIndicator!
+    
+    var nowPlayingTimer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTimer()
+    }
+}
