b3d00b241dd76f6ec4ae6ef69ddca209649eaf41
krishpranav <krisna.pranav@gmail.com>
2021-05-21 09:30:33 +0530

    [+] Added title, uri added [+]

--- lyricsify/Track.swift
+++ lyricsify/Track.swift
@@ -16,4 +16,17 @@ class Track {
     public var name:   String
     public var lyrics: String?
     public var onLyricsChanged: ((String) -> Void)?
+    
+    public var title: String {
+        get {
+            if self.artist != "" && self.lyrics "" {
+                return "\(self.artist) - \(self.name)"
+            }
+            return ""
+        }
+    }
+    
+    private let searchApiUri = "http://lyrics.wikia.com/index.php"
+    private let lyricsUriPath = "http://lyrics.wikia.com/wiki/"
+    
 }
